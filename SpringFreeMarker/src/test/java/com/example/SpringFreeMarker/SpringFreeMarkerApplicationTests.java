be701d1f995e03e30b19b0c9902af9ae536b9507
Anupatil6444 <anupatil6444@gmail.com>
2023-08-08 19:27:24 +0530

    Files Added

---
+++ SpringFreeMarker/src/test/java/com/example/SpringFreeMarker/SpringFreeMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringFreeMarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFreeMarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
